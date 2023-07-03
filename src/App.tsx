42fde8da171942c32f82c42584bdb6b7efbc663a
Sebastião Rodrigo <rodrigoexer1@gmail.com>
2023-07-03 16:10:08 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="container">
+      <h1>Modelo React TS + Bootstrap</h1>
+      <button className="btn btn-primary">Hello Boostra!</button>
+    </div>
+  )
+}
+
+export default App
